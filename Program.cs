14829628cfb7c7fa147c2148d07a0ef94508a544
Samuel Hipólito <samuelhplt@gmail.com>
2024-10-27 22:30:05 -0300

    Implementação de Merge Sort e Quick Sort

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using sort_algorithms;
+
+Console.WriteLine("Hello, World!");
+var sorting = new Sorting();
+
+Random random = new Random();
+var anyNumbers = Enumerable.Range(1, 999)
+                           .OrderBy(x => random.Next())
+                           .Take(42)
+                           .ToArray();
+                           
+int[] inversedOrder = new int[] { 117, 90, 88, 83, 81, 77, 74, 69, 64, 63, 51, 
+                                50, 49, 42, 41, 34, 32, 29, 28, 22, 16, 8,
+                                6, 5, 3, 1};
+
+int[] repeatedNumbers = new int[] { 7, 7, 7, 7, 7, 1, 1, 9, 9, 0, 4, 4, 4, 5, 4, 5, 7, 1 };             
+
+Console.WriteLine("------ Sem Ordenar ------");
+Console.Write("-> ");
+foreach (int number in anyNumbers)
+    Console.Write(number + " ");
+Console.Write("\n-> ");
+foreach (int number in inversedOrder)
+    Console.Write(number + " ");
+Console.Write("\n-> ");
+foreach (int number in repeatedNumbers)
+    Console.Write(number + " ");
+
+// Merge Sorting
+/*
+Console.WriteLine("\n------ Com Merge Sort ------");
+sorting.MergeSort(anyNumbers);
+sorting.MergeSort(inversedOrder);
+sorting.MergeSort(repeatedNumbers);
+// Números Aleatórios
+Console.Write("-> ");
+foreach (int number in anyNumbers)
+    Console.Write(number + " ");
+Console.WriteLine();
+// Números ordenados em ordem decrescente
+Console.Write("-> ");
+foreach (int number in inversedOrder)
+    Console.Write(number + " ");
+// Números repetidos
+Console.Write("\n-> ");
+foreach (int number in repeatedNumbers)
+    Console.Write(number + " "); */
+
+// Quick Sorting
+Console.WriteLine("\n------ Com Quick Sort ------");
+sorting.QuickSort(repeatedNumbers);
+foreach (int num in repeatedNumbers)
+    Console.Write(num + " ");
+    
---
+++ Sorting.cs
@@ -0,0 +1,86 @@
+﻿namespace sort_algorithms;
+
+public class Sorting
+{
+    public void MergeSort(int[] lista, int inicio = 0, int fim = -1)
+    {
+        if (fim == -1)
+            fim = lista.Length;
+        
+        if ((fim - inicio) > 1)
+        {
+            int meio = (fim + inicio) / 2;
+            MergeSort(lista, inicio, meio);
+            MergeSort(lista, meio, fim);
+            Merge(lista, inicio, meio, fim);
+        }
+    }
+
+    private void Merge(int[] lista, int inicio, int meio, int fim)
+    {
+        int[] left = new int[meio - inicio];
+        int[] right = new int[fim - meio];
+
+        for(int i = 0; i < left.Length; i++)
+            left[i] = lista[inicio + i];
+        for (int i = 0; i < right.Length; i++)
+            right[i] = lista[meio + i];
+
+        int topLeft = 0, topRight = 0;
+
+        for (int k = inicio; k < fim; k++)
+        {
+            if (topLeft >= left.Length)
+            {
+                lista[k] = right[topRight];
+                topRight++;
+            }
+            else if(topRight >= right.Length)
+            {
+                lista[k] = left[topLeft];
+                topLeft++;
+            }
+            else if (left[topLeft] > right[topRight])
+            {
+                lista[k] = right[topRight];
+                topRight++;
+            }
+            else
+            {
+                lista[k] = left[topLeft];
+                topLeft++;
+            }
+        }
+    }
+
+    public void QuickSort(int[] lista, int inicio = 0, int fim = -1)
+    {
+        if (fim == -1)
+            fim = lista.Length - 1; // Tira a última posição, pq a última posição é o pivô.
+
+        if (fim > inicio)
+        {
+            int pivot = Partition(lista, inicio, fim);
+            QuickSort(lista, inicio, pivot - 1);
+            QuickSort(lista, pivot + 1, fim);
+        }
+    }
+
+    private int Partition(int[] lista, int inicio, int fim)
+    {
+        int pivot = lista[fim];
+        int i = inicio;
+
+        for (int j = inicio; j < fim; j++)
+        {
+            if (lista[j] <= pivot)
+            {
+                (lista[i], lista[j]) = (lista[j], lista[i]);
+                i++;
+            }
+        }
+        
+        (lista[i], lista[fim]) = (lista[fim], lista[i]);
+        return i;
+    }
+}
